139100de5d9a368ddf33b0b759935cce576479a0
soyiss <zwcrvl@naver.com>
2023-03-14 12:54:09 +0900

    구구단

---
+++ src/ex/Example1.java
@@ -0,0 +1,14 @@
+package ex;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=9;i++) {
+			for(int j=1;j<=9;j++) {
+				System.out.println(i+"*"+j+"="+(i*j));
+			}
+		}
+	}
+
+}
