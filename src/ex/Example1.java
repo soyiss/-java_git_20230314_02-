9fa1061d6658d11460415848b358ab6e9caa3343
soyiss <zwcrvl@naver.com>
2023-03-14 13:27:06 +0900

    구구단수정본

--- src/ex/Example1.java
+++ src/ex/Example1.java
@@ -1,14 +1,12 @@
 package ex;
 
 public class Example1 {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<=9;i++) {
-			for(int j=1;j<=9;j++) {
-				System.out.println(i+"*"+j+"="+(i*j));
-			}
-		}
+		Example2 example2 = new Example2();
+		
+		example2.numberTable();
 	}
 
 }
---
+++ src/ex/Example2.java
@@ -0,0 +1,19 @@
+package ex;
+
+public class Example2 {
+
+	
+	public void numberTable(){
+		for(int i=1;i<=9;i++) {
+			System.out.println("\n"+i + "단");
+			for(int j=1;j<=9;j++) {
+				System.out.println(i+"*"+j+"="+(i*j));
+			}
+		}
+	}
+	
+	
+	
+	
+	
+}
